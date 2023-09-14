bc0847b9e4218231652306723d7323e744361a11
Nalani Daniels <nalanidaniels23@gmail.com>
2023-09-14 22:41:16 +0000

    created new python file

---
+++ vowels.py
@@ -0,0 +1,6 @@
+def removeVowels(string):
+   vowels = ["a", "e", "i", "o", "u", "y"]
+   for letter in string:
+        if letter.lower() in vowels:
+            string = string.replace(letter, "")
+   return string
